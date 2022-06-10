3eaed03ab34a0175e1d0d5168da14e9d0426405d
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-06-10 23:42:25 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
-
+import '../assets/css/font-awesome.min.css'
+import '../assets/css/linearicons.css'
+import '../assets/css/style.css'
+import '../assets/css/animate.css'
+import '../assets/css/bootstrap.min.css'
+import '../assets/css/bootsnav.css'
+import '../assets/css/responsive.css'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+/*import '../https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i'*/
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
